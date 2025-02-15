61f6698e5193ad2ebcce802cb357861163acb7a7
Hariwar <105588706+Hariwar@users.noreply.github.com>
2025-02-15 22:15:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 import re
 from os import environ
 import os
@@ -6,20 +5,19 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = os.environ.get("API_ID", "10811400")
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAEelXXHhNoWXR_duKpDJiFl39LbI7A3sU0")
-ADMIN = int(os.environ.get("ADMIN", '6469754522'))
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
-DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://UPLOADXPRO24BOT:UPLOADXPRO24BOT@cluster0.hjfk60f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+API_ID = os.environ.get("API_ID", "17822592")
+API_HASH = os.environ.get("API_HASH", "a20b3dbbe07ed695563b4609a3e62012")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7694688386:AAEyyOnqVujrBBi12eE6THWmhS8C9GG_tRw")
+ADMIN = int(os.environ.get("ADMIN", '7835575911'))
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "mtpmasala")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "mtpmasala")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://temaga6324:rolex143@cluster0.hinoe.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 CAPTION = os.environ.get("CAPTION", "")
-group = environ.get('GROUP', '-1002173560131')
+group = environ.get('GROUP', '-1002397670865')
 GROUP = int(group) if group and id_pattern.search(group) else None
-#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
-SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+SUNRISES_PIC= "https://telegra.ph/file/0151dc084b62cb01b209d.jpg"  # Replace with your Telegraph link
+AUTH_USERS = int(os.environ.get("AUTH_USERS", '7835575911'))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 PORT = int(os.environ.get("PORT", "8080"))
-LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002145984196)
+LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", "-1002326958626")
